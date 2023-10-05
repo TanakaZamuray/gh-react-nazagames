509a3aee63ed8147869f9dbf61d4d7c15ba6bf2f
Tavo Quintana <xtremoc.log@gmail.com>
2023-10-05 18:05:20 -0500

    Fix Url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https//gh-react-nazagames.github.io/",
+  base: "https//tanakazamuray.github.io/gh-react-nazagames/",
   plugins: [react()],
 })
