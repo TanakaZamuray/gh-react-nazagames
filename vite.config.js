d37c6dd1a0c041cb95a7e352b1d52b77595b2c01
Tavo Quintana <xtremoc.log@gmail.com>
2023-10-05 17:34:13 -0500

    Add Scritp + Dev

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://tanakazamuray.github.io/',
+  base: "/gh-react-nazagames/",
   plugins: [react()],
 })
